e02c331671e979c9e87df995eae37cfaf8bf353f
塩/Salt <nabb9771@icloud.com>
2025-12-07 21:46:49 +0900

    Add audio.js to load audio files

---
+++ audio.js
@@ -0,0 +1,7 @@
+const audios = [
+    new Audio('../audio/鳩時計1.mp3'),
+    new Audio('../audio/キーボード1.mp3'),
+    new Audio('../audio/DJのスクラッチ1.mp3'),
+    new Audio('../audio/建物が少し崩れる1.mp3'),
+    new Audio('../audio/建物が少し崩れる2.mp3'),
+];
